626a8d3a786e76d5bf79cacb56e51cab89348ab9
Eric Laursen <ericjlaursen@gmail.com>
2018-07-23 21:08:51 -0400

    Basic endpoint.

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
-const express = require('express')
 const app = express();
-const { PORT, DATABASE_URL } = require("./config");
-
-app.use(express.json());
-
-// CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});
+const PORT = process.env || 3000;
 
 app.get('/api/*', (req,res) => {
-	res.json({ok: true})
+  res.json({ok: true})
 })
 
 app.get('/jazzCharts', (req,res) => {
-	res.json({"willBe":"JazzCharts"})
+  res.json({willbe:"JazzCharts soon!"})
 })
 
-// catch-all endpoint if client makes request to non-existent endpoint
-app.use("*", function(req, res) {
-  res.status(404).json({ message: "Not Found" });
-});
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`)
+})
 
-module.exports = { app }
+module.exports = {app}
